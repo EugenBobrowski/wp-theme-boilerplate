dfa6daa9cf28b331284ee7348b748d9c44fd75ad
eugen <ecologistkai@gmail.com>
2016-01-26 14:33:02 +0200

    Update renamer.php

--- renamer.php
+++ renamer.php
@@ -17,7 +17,10 @@ class Renamer {
      * @var string
      */
     public $replace;
-
+    /**
+     * @var array
+     */
+    public $ignore = array();
     /**
      * @var array. Keys are searched. Values are replaced.
      */
@@ -25,6 +28,11 @@ class Renamer {
 
     public function __construct() {
 
+    }
+
+    public function addIgnore($file)
+    {
+        $this->ignore[] = $file;
     }
 
     public function generate_replace_array() {
@@ -74,6 +82,11 @@ class Renamer {
                 echo '<li>';
                 echo '<em>THIS FILE '.__FILE__.'</em>';
                 echo '</li>';
+            }  elseif (in_array($path_item, $this->ignore)) {
+                echo '<li>';
+                echo '<em>' . $item . '</em>';
+                echo ' [IGNORED] ';
+                echo '</li>';
             } elseif (is_dir($path_item)) {
                 echo '<li>';
                 echo '<strong>'.$item.'</strong>';
@@ -112,6 +125,38 @@ class Renamer {
 
 }
 
+
+
+?>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+        <title>Boilerplate Renamer</title>
+
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+              integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css"
+              integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+
+        <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+    <h1>Boilerplate Renamer</h1>
+
+<?php
+
 $renameObject = new Renamer();
 //
 //$renameObject->replace_array = array(
@@ -129,9 +174,23 @@ $renameObject = new Renamer();
 $renameObject->search = 'Twenty Fifteen';
 $renameObject->replace = 'Twenty Something';
 $renameObject->generate_replace_array();
+$renameObject->addIgnore('.//.git');
 
 /**
  * Run the renaming process
  */
 
 $renameObject->scanTheDir('./');
+
+
+?>
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+            integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+            crossorigin="anonymous"></script>
+    </body>
+</html>
