dba7d40c6b5b2f9265fc39effd0190f77db06bb2
eugen <ecologistkai@gmail.com>
2016-01-26 19:41:51 +0200

    Add woo commerce support

--- functions.php
+++ functions.php
@@ -296,3 +296,10 @@ require get_template_directory() . '/inc/template-tags.php';
  * @since Twenty Fifteen 1.0
  */
 require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Customize Woocommerce and integrate.
+ *
+ * @since Twenty Fifteen 1.0
+ */
+require get_template_directory() . '/inc/woo-customizing.php';
